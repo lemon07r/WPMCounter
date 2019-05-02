38dfeffba419933978f30486a4ae6794a076c42c
Lamim <lemon07r@gmail.com>
2019-05-02 15:20:33 -0400

    Code cleanup

--- counter.js
+++ counter.js
@@ -1,7 +1,8 @@
+'use strict';
+
 const win = nw.Window.get();
 win.setAlwaysOnTop(true);
 
-'use strict';
 const ioHook = require('iohook');
 const keyCodeWhiteList = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 55, 57, 74, 78,
